23a180dffa9e5ef80ce43d0126ed7ec8b9f56593
Eric Ray <erichamion@gmail.com>
2022-04-14 18:43:58 -0500

    Add project files.

---
+++ ArraysScratchpad/Program.cs
@@ -0,0 +1,77 @@
+﻿int[] myIntArray = new int[7];
+
+myIntArray[3] = 27;
+myIntArray[5] = 35;
+
+int sum = myIntArray[3] + myIntArray[5];
+
+string[] myStringArray = new string[2];
+
+myStringArray[0] = "Hello";
+myStringArray[1] = "World";
+
+Console.WriteLine(myStringArray[1]);
+Console.WriteLine(myStringArray[0]);
+
+
+
+int[] literalArray;
+
+//literalArray = new int[5];
+//literalArray[0] = 27;
+//literalArray[1] = 31;
+//literalArray[2] = 97;
+//literalArray[3] = 100;
+//literalArray[4] = 35;
+
+
+literalArray = new int[] { 27, 31, 97, 100, 35, 80 };
+
+
+
+literalArray[6] = 7;
+
+for (int i = 0; i < literalArray.Length; ++i)
+{
+    Console.WriteLine(literalArray[i]);
+}
+
+//////////////////////////////////////////////////////////////
+
+// Get 3 strings from the user.
+// Print them in reverse order.
+
+
+// DRY:
+// Don't
+// Repeat
+// Yourself
+
+Console.WriteLine("How many strings?");
+string numberAsString = Console.ReadLine();
+int number = int.Parse(numberAsString);
+
+string[] inputs = new string[number];
+
+for(int i = 0; i < inputs.Length; ++i)
+{
+    Console.WriteLine("Enter a string:");
+    inputs[i] = Console.ReadLine();
+}
+
+for(int i = inputs.Length - 1; i >= 0; --i)
+{
+    Console.WriteLine(inputs[i]);
+}
+
+foreach(string input in inputs)
+{
+    Console.WriteLine(input);
+}
+
+
+
+
+
+
+Console.ReadKey();
